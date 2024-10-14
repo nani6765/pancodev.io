b361ddfed792f6d2f3e147d8f0656f00f376b9d2
Pandacoding <nani6765@gmail.com>
2024-10-14 21:22:35 +0900

    chore: init remix app

---
+++ app/root.tsx
@@ -0,0 +1,57 @@
+import {
+  Form,
+  Links,
+  Meta,
+  Scripts,
+  ScrollRestoration,
+  Outlet,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder="Search"
+                type="search"
+              />
+              <div aria-hidden hidden={true} id="search-spinner" />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href={`/contacts/1`}>Your Name</a>
+              </li>
+              <li>
+                <a href={`/contacts/2`}>Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
