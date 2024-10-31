98b37361e104fd30d4574ce11fd814b6d62c05ef
이재원 <55654872+nani6765@users.noreply.github.com>
2024-10-31 10:25:06 +0900

    Update validatePublicationDate.ts

--- script/function/validatePublicationDate.ts
+++ script/function/validatePublicationDate.ts
@@ -1,16 +1,7 @@
 import dayjs from "dayjs";
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
-
-import 'dayjs/locale/ko';
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.tz.setDefault('Asia/Seoul');
-dayjs.locale('ko');
 
 const validatePublicationDate = (createdAt: string) => {
-  const today = dayjs();
+  const today = dayjs(new Date().toLocaleDateString('ko-kr'));
   const publish = dayjs(createdAt)
   
   console.log('today : ', today.format('YYYY-MM-DD HH:mm'));
