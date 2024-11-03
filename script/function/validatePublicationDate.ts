d621ac6abf56641df83e5b98d20b67b2d2cf4d31
Pandacoding <nani6765@gmail.com>
2024-11-03 20:59:24 +0900

    feat: utc logging

--- script/function/validatePublicationDate.ts
+++ script/function/validatePublicationDate.ts
@@ -3,6 +3,8 @@ import dayjs from "@/util/dayjs.helper";
 const validatePublicationDate = (createdAt: string) => {
   const today = dayjs.utc();
   const publish = dayjs(createdAt).utc();
+  console.log("today : ", today.format("YYYY-MM-DD"));
+  console.log("publish : ", publish.format("YYYY-MM-DD"));
 
   return today.isAfter(publish);
 };
