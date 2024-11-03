83489ab0400351b4fff903a494a2c70df7cb6591
Pandacoding <nani6765@gmail.com>
2024-11-03 20:41:53 +0900

    feat: vercel deploy timezone check

--- script/function/validatePublicationDate.ts
+++ script/function/validatePublicationDate.ts
@@ -2,6 +2,7 @@ import dayjs from "@/util/dayjs.helper";
 
 const validatePublicationDate = (createdAt: string) => {
   const today = dayjs(new Date());
+  console.log(today.format("YYYY-MM-DD HH:mm:ss"));
   const publish = dayjs(createdAt);
   return today.isAfter(publish);
 };
