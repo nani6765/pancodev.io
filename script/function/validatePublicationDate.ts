270674519a6af82198312fb01cbd6c25722cecd5
Pandacoding <nani6765@gmail.com>
2024-11-03 21:01:03 +0900

    feat

--- script/function/validatePublicationDate.ts
+++ script/function/validatePublicationDate.ts
@@ -3,8 +3,8 @@ import dayjs from "@/util/dayjs.helper";
 const validatePublicationDate = (createdAt: string) => {
   const today = dayjs.utc();
   const publish = dayjs(createdAt).utc();
-  console.log("today : ", today.format("YYYY-MM-DD"));
-  console.log("publish : ", publish.format("YYYY-MM-DD"));
+  console.log("today : ", today.format("YYYY-MM-DD HH:mm"));
+  console.log("publish : ", publish.format("YYYY-MM-DD HH:mm"));
 
   return today.isAfter(publish);
 };
